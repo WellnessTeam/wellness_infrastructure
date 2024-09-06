caee6c2962cb59fd23fd6089bb6828cb4d5f4b54
노석현 <shtjrgus010@github.com>
2024-09-06 15:47:17 +0900

    setup: vpc

--- state_resource.tf
+++ state_resource.tf
@@ -3,12 +3,16 @@
 resource "aws_s3_bucket" "tfstate" {
   bucket = "wellenss-infra-terraform"
 
-  versioning {
-    enabled = true
-  }
-
   tags = {
     Name = "Terraform State Storage"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tfstate_versioning" {
+  bucket =aws_s3_bucket.tfstate.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
