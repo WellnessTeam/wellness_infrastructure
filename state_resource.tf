c1d8378b0e0c1a1c1b8828bc50b9217266224c0e
노석현 <shtjrgus010@github.com>
2024-09-06 15:48:45 +0900

    setup: vpc

--- state_resource.tf
+++ state_resource.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "tfstate" {
 }
 
 resource "aws_s3_bucket_versioning" "tfstate_versioning" {
-  bucket =aws_s3_bucket.tfstate.id
+  bucket = aws_s3_bucket.tfstate.id
 
   versioning_configuration {
     status = "Enabled"
