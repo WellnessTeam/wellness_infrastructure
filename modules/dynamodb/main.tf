65922b4d81c0529ddaeb9483031607c998a1e94a
노석현 <shtjrgus010@github.com>
2024-09-07 21:42:30 +0900

    setup: eks

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -2,6 +2,10 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
   name         = var.dynamodb_table_name
   hash_key     = "LockID"
   billing_mode = "PAY_PER_REQUEST"
+  
+  lifecycle {
+    prevent_destroy = true
+  }
 
   attribute {
     name = "LockID"
--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,6 +1,10 @@
 resource "aws_s3_bucket" "tfstate" {
   bucket = var.s3_bucket_name
 
+  lifecycle {
+    prevent_destroy = true
+  }
+  
   tags = {
     Name = "Terraform State Storage"
   }
