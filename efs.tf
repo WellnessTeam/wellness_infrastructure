08f9b63d3d89ef071039bbe45de8e6b33b3b1a18
노석현 <shtjrgus010@github.com>
2024-09-07 20:14:19 +0900

    setup: eks

--- efs.tf
+++ efs.tf
@@ -1,13 +1,13 @@
 resource "aws_efs_file_system" "efs" {
-    tags = {
-        Name = "${var.cluster_base_name}-EFS"
-    }
+  tags = {
+    Name = "${var.cluster_base_name}-EFS"
+  }
 }
 
 resource "aws_efs_mount_target" "efs_mount_target" {
-    count = length(var.public_subnet_blocks)
+  count = length(var.public_subnet_blocks)
 
-    file_system_id = aws_efs_file_system.efs.id
-    subnet_id = aws_subnet.public_subnet[count.index].id
-    aws_security_groups = [aws_security_group.efs_sg.id]
+  file_system_id      = aws_efs_file_system.efs.id
+  subnet_id           = aws_subnet.public_subnet[count.index].id
+  aws_security_groups = [aws_security_group.efs_sg.id]
 }
