b14a79a69445770d9646dde2bc1485c7d67f12f2
노석현 <shtjrgus010@github.com>
2024-09-09 16:05:52 +0900

    좀 돼라

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -48,9 +48,13 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.eks_vpc.id
-
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
   tags = {
-    Name = "${var.cluster_base_name}-public-route-table"
+    Name = "${var.cluster_base_name}-PublicRouteTable"
   }
 }
 
@@ -64,4 +68,18 @@ resource "aws_route_table_association" "public_rt_association" {
   count          = 3
   subnet_id      = aws_subnet.public_subnets[count.index].id
   route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "${var.cluster_base_name}-PrivateRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_association" {
+  count = 3
+  subnet_id = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
 }
